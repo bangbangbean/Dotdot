645f3d7a5065607f0ae44e58c6956275bcbdc66b
bangbangbean <jack0001515@gmail.com>
2020-02-03 16:14:41 +0800

    S

---
+++ app/src/main/java/com/example/dotdot/Forgetpassword.java
@@ -0,0 +1,14 @@
+package com.example.dotdot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Forgetpassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgetpassword);
+    }
+}
