7b2668e9e122c78f857bf6eb43e89397fa799912
bangbangbean <jack0001515@gmail.com>
2020-02-04 15:43:46 +0800

    S

--- app/src/main/java/com/example/dotdot/Regist.java
+++ app/src/main/java/com/example/dotdot/Regist.java
@@ -5,16 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
 
+import com.google.firebase.functions.FirebaseFunctions;
+
 public class Regist extends AppCompatActivity {
 
     private EditText phone;
     private EditText password;
     private EditText passagain;
     private EditText username;
-    private EditText erqwer;
+    private EditText birthday;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_regist);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        phone=(EditText)findViewById(R.id.phone);
+        password=(EditText)findViewById(R.id.password);
+        passagain=(EditText)findViewById(R.id.passagain);
+        username=(EditText)findViewById(R.id.username);
+        birthday=(EditText)findViewById(R.id.birthday);
+
     }
 }
