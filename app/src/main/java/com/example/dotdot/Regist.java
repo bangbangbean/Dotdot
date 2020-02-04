d1e4142f9daca8ae8125ddda2097cef00b110c95
bangbangbean <jack0001515@gmail.com>
2020-02-04 14:32:14 +0800

    S

--- app/src/main/java/com/example/dotdot/Regist.java
+++ app/src/main/java/com/example/dotdot/Regist.java
@@ -3,9 +3,15 @@ package com.example.dotdot;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class Regist extends AppCompatActivity {
 
+    private EditText phone;
+    private EditText password;
+    private EditText passagain;
+    private EditText username;
+    private EditText erqwer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
