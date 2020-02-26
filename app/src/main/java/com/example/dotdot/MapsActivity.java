41e3f9e78f0367770b2ecef60fc8401ff0e05eb4
Linchenzen <kenny19990428@gmail.com>
2020-02-26 14:42:57 +0800

    map

--- app/src/main/java/com/example/dotdot/MapsActivity.java
+++ app/src/main/java/com/example/dotdot/MapsActivity.java
@@ -17,7 +17,6 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 import java.util.ArrayList;
